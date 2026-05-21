23ce77b5e2de9d553780a011c1db0466b8c2474d
EaindraKyawWin <eaindrakyawwin@globalwave.company>
2026-05-21 10:47:05 +0630

    update_version1

--- database/migrations/2026_04_30_164729_create_order_items_table.php
+++ database/migrations/2026_04_30_164729_create_order_items_table.php
@@ -6,10 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+       public function up(): void
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
@@ -21,9 +18,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         Schema::dropIfExists('order_items');
